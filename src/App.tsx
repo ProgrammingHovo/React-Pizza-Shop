45ebd47f1201dfd339884c1ed11594f95d1ade9e
Hovhannes Ghaltakhchyan <hovo.program@gmail.com>
2022-12-12 20:40:30 +0400

    Splitted the code with React.lazy

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,49 @@
-import React from "react";
+import React, { Suspense } from "react";
 import "./scss/app.scss";
 import { Routes, Route } from "react-router-dom";
 
 import MainLayout from "./layouts/MainLayout";
-
-import FullPizza from "./pages/FullPizza";
 import Home from "./pages/Home";
-import CartEmpty from "./pages/Cart";
-import NotFound from "./pages/NotFound";
+
+const Cart = React.lazy(
+  () => import(/* webpackChunkName: "Cart"*/ "../src/pages/Cart")
+);
+const FullPizza = React.lazy(
+  () => import(/* webpackChunkName: "FullPizza"*/ "../src/pages/FullPizza")
+);
+const NotFound = React.lazy(
+  () => import(/* webpackChunkName: "NotFound"*/ "../src/pages/NotFound")
+);
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<MainLayout />}>
         <Route path="" element={<Home />} />
-        <Route path="pizza/:id" element={<FullPizza />} />
-        <Route path="cart" element={<CartEmpty />} />
-        <Route path="*" element={<NotFound />} />
+        <Route
+          path="pizza/:id"
+          element={
+            <Suspense fallback={<div>Loading...</div>}>
+              <FullPizza />
+            </Suspense>
+          }
+        />
+        <Route
+          path="cart"
+          element={
+            <Suspense fallback={<div>Loading...</div>}>
+              <Cart />
+            </Suspense>
+          }
+        />
+        <Route
+          path="*"
+          element={
+            <Suspense fallback={<div>Loading...</div>}>
+              <NotFound />
+            </Suspense>
+          }
+        />
       </Route>
     </Routes>
   );
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
 //        HashRouter
 
@@ -24,4 +23,3 @@ if (rootElement) {
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
